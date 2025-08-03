1ee18669f908a3bae8b14e9de29f1b1b50666ac1
Leonid Vlasov <leoniv.vlasov@gmail.com>
2025-08-03 15:42:21 +0700

    Add look deeper

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-	. "local/go-exercises/data/list"
-	"local/go-exercises/yandex"
+	lookdeeper "local/go-exercises/look_deeper"
 	"math"
 )
 
@@ -12,13 +10,5 @@ const (
 )
 
 func main() {
-	list := func() *List[int] { return MakeList([]int{1, 2, 3, 4, 5, 6, 7}) }
-	fmt.Println(list().ToSlice())
-	fmt.Println(yandex.IsItPolindrom(list()))
-	ll := MakeList([]int{1, 2, 3})
-	lr := MakeList([]int{4, 3, 2, 6})
-	fmt.Println(ll, lr)
-	fmt.Println(Zip(ll, lr).ToSlice(), ll, lr)
-	zero := 1
-	fmt.Println(Fold(list(), zero, func(a int, b int) int { return a * b }), zero)
+	lookdeeper.Run()
 }
---
+++ look_deeper/examples.go
@@ -0,0 +1,58 @@
+package lookdeeper
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func Run() {
+	fmt.Println("Looking deeper to:")
+	lookToSlice()
+	lookToMap()
+	lookIntoPointers()
+}
+
+func lookIntoPointers() {
+	var ip *int
+	var ipp **int = &ip
+	fmt.Println("Look into pointers (var ip *int)", "pointer var address:", ipp, "pointer:", ip, "pointed value:", "")
+	ip = new(int)
+	fmt.Println("Look into pointers (var ip *int)", "pointer var address:", ipp, "pointer:", ip, "pointed value:", **ipp)
+	*ip = 10
+	fmt.Println("Look into pointers (var ip *int)", "pointer var address:", ipp, "pointer:", ip, "pointed value:", *ip)
+
+	var xsp *[]int
+	fmt.Println("Look into pointers (var xsp *[]int)", "pointer var address:", &xsp, "pointer:", xsp, "pointed value:", "")
+	xsp = new([]int)
+	fmt.Println("Look into pointers (var xsp *[]int)", "pointer var address:", &xsp, "pointer:", xsp, "pointed value:", *xsp)
+	var xspp *[]int
+	tmp := append(*xsp, 42)
+	xspp = &tmp
+	fmt.Println("Look into pointers (xspp := append(*xsp, 42)", "pointer var address:", &xspp, "pointer:", xspp, "pointed value:", *xspp)
+
+}
+
+func lookToMap() {
+	map_ := make(map[int]int)
+
+	fmt.Println("Look to map (new map):", unsafe.Pointer(&map_), map_)
+	lookToMapDownStream(map_)
+	fmt.Println("Look to map (down stream return):", unsafe.Pointer(&map_), map_)
+}
+
+func lookToMapDownStream(in map[int]int) {
+	in[42] = 24
+	fmt.Println("Look to map (down stream):", unsafe.Pointer(&in), in)
+}
+
+func lookToSlice() {
+	var slice []int
+	fmt.Println("Look to slice (new slice):", unsafe.Pointer(&slice), cap(slice), slice)
+	lookToSliceDownStream(slice)
+	fmt.Println("Look to slice (down stream return):", unsafe.Pointer(&slice), cap(slice), slice)
+}
+
+func lookToSliceDownStream(in []int) {
+	in = append(in, 10)
+	fmt.Println("Look to slice (down stream):", unsafe.Pointer(&in), cap(in), in)
+}
